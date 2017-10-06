915a99d49b088729d398afe5a5d71333187210d1
Will H McMahan <will@mcmahan.me>
2017-10-06 11:21:24 -0400

    Add gscan to gulp deploy as validator
    Using [gscan](https://github.com/TryGhost/gscan), the gulp task deploy will now
    generate a zip file and then scan the zip file for compliance with ghost. Any
    issues will be printed to the console. This implimentation borrows heavily from
    gscan's cli implimentation, which can be found at
    [gscan/bin/cli.js](https://github.com/TryGhost/gscan/blob/1.2.0/bin/cli.js).

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
-var autoprefix = require("gulp-autoprefixer"),
+var _          = require("lodash"),
+    autoprefix = require("gulp-autoprefixer"),
     bourbon    = require("bourbon").includePaths,
+    chalk      = require('chalk'),
+    gscan      = require("gscan"),
     gulp       = require("gulp"),
     jshint     = require("gulp-jshint"),
     neat       = require("bourbon-neat").includePaths,
+    run        = require("run-sequence"),
     sass       = require("gulp-sass"),
-    stylish    = require("jshint-stylish");
+    stylish    = require("jshint-stylish"),
+    zip        = require("gulp-zip"),
+    levels;
 
 var paths = {
   scss: "./assets/scss/**/*.scss",
@@ -12,7 +18,12 @@ var paths = {
   js: "./assets/js/**/*.js"
 };
 
-// Stylesheets
+levels = {
+  error: chalk.red,
+  warning: chalk.yellow,
+  recommendation: chalk.yellow,
+  feature: chalk.green
+};
 
 gulp.task("sass", function () {
   return gulp.src(paths.scss)
@@ -24,17 +35,62 @@ gulp.task("sass", function () {
     .pipe(gulp.dest(paths.css));
 });
 
-// Javascript
-
 gulp.task("jshint", function () {
   gulp.src(paths.js)
     .pipe(jshint())
     .pipe(jshint.reporter("jshint-stylish"));
 });
 
-// Tasks
+gulp.task("scan", function() {
+  function outputResult(result) {
+    console.log('-', levels[result.level](result.level), result.rule);
+  }
+
+  function outputResults(theme) {
+    theme = gscan.format(theme);
+
+    console.log(chalk.bold.underline('\nRule Report:'));
+
+    if (!_.isEmpty(theme.results.error)) {
+      console.log(chalk.red.bold.underline('\n! Must fix:'));
+      _.each(theme.results.error, outputResult);
+    }
+
+    if (!_.isEmpty(theme.results.warning)) {
+      console.log(chalk.yellow.bold.underline('\n! Should fix:'));
+      _.each(theme.results.warning, outputResult);
+    }
+
+    if (!_.isEmpty(theme.results.recommendation)) {
+      console.log(chalk.red.yellow.underline('\n? Consider fixing:'));
+      _.each(theme.results.recommendation, outputResult);
+    }
+
+    if (!_.isEmpty(theme.results.pass)) {
+      console.log(chalk.green.bold.underline('\n\u2713', theme.results.pass.length, 'Passed Rules'));
+    }
+
+    console.log('\n...checks complete.');
+  }
+
+  return gscan.checkZip({
+    path: "./ghost-theme-template.zip",
+    name: "ghost-theme-template"
+  })
+  .then(outputResults);
+});
+
+gulp.task("zip", function() {
+  return gulp.src(["./**/*", "!./node_modules/**"])
+    .pipe(zip("ghost-theme-template.zip"))
+    .pipe(gulp.dest("."));
+});
 
 gulp.task("default", ["sass", "jshint"], function() {
   gulp.watch(paths.scss, ["sass"]);
   gulp.watch(paths.js, ["jshint"]);
+});
+
+gulp.task("deploy", function(callback) {
+  run("zip", "scan", callback);
 });
